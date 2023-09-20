ea33815dbc9817eff0e8bf5fc4043e8ceeac3497
Amit Waghmare <120040137+AmitBharatWaghmare@users.noreply.github.com>
2023-09-20 20:18:23 +0530

    Delete utils directory

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-
-const Sequelize =require(`sequelize`);
-const sequelize= new Sequelize(`expensesapp`,`root`,`@mIT1995`,{
-    dialect:`mysql`,
-    host:`localhost`
-  
-});
-module.exports=sequelize;
